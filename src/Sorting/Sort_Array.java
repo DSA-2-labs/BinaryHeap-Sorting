16326ee8e3aa688e0cf87cccd9d428a60887ba2e
Hussein Khaled <husseinkhaled733@gmail.com>
2023-05-26 19:54:44 +0300

    uploading merge_sort

--- src/Sorting/Sort_Array.java
+++ src/Sorting/Sort_Array.java
@@ -6,17 +6,17 @@ import java.util.ArrayList;
 
 public class Sort_Array {
     private int[] Unsorted_Array;
+    private ArrayList<int[]> list = new ArrayList<>();
     public Sort_Array(String filename)
     {
         this.Unsorted_Array= Freader.read(filename);
     }
     public Object Simple_Sort(boolean choice)
     {
-        return bubble_sort(this.Unsorted_Array.clone(),choice);
+        return merge_sort(this.Unsorted_Array.clone(),choice,0,this.Unsorted_Array.length-1);
     }
     private Object bubble_sort(int[] unsorted,boolean choice)
     {
-        ArrayList<int[]> list = new ArrayList<>();
         list.add(unsorted.clone());
         for (int i = unsorted.length; i > 1; i--)
         {
@@ -28,6 +28,50 @@ public class Sort_Array {
         }
         return choice ? list.get(list.size()-1) : list.toArray(new int[0][]);
     }
+    private Object merge_sort(int[] unsorted,boolean choice,int l,int r)
+    {
+        if(l<r){
+            int mid=(l+r)/2;
+            merge_sort(unsorted,choice,l,mid);
+            merge_sort(unsorted,choice,mid+1,r);
+            int[] left=new int[mid-l+1];
+            int[] right=new int[r-mid];
+            for (int i = 0; i < left.length ; i++) {
+                left[i]=unsorted[i+l];
+            }
+            for (int i = 0; i < right.length ; i++) {
+                right[i]=unsorted[i+mid+1];
+            }
+            merge(unsorted,left,right,l);
+
+        }
+        return choice ? list.get(list.size()-1) : list.toArray(new int[0][]);
+    }
+    public void merge(int[] unsorted,int[] left,int[] right,int l){
+        int nl= left.length;
+        int nr= right.length;
+        int k=l,i=0,j=0;
+        while(i<nl&&j<nr){
+            if(left[i]<right[j]){
+                unsorted[k]=left[i];
+                k++;i++;
+            }
+            else{
+                unsorted[k]=right[j];
+                k++;j++;
+            }
+        }
+        while(i<nl){
+            unsorted[k]=left[i];
+            k++;i++;
+        }
+        while(j<nr){
+            unsorted[k]=right[j];
+            k++;j++;
+        }
+        list.add(unsorted.clone());
+    }
+
     private void swap(int index,int[] arr)
     {
         int temp = arr[index];
