ab0cc4239204ab761c47f5bc6a187ff6162258a6
AhmedAli3011 <128240330+AhmedAli3011@users.noreply.github.com>
2023-05-26 23:03:00 +0300

    Add files via upload

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,26 +1,33 @@
+import java.util.*;
+
 public class MaxHeap {
     int[] heapArray;
     int size;
 
-    private static void heapify_up(int[] array,int size,int index){
+     static private void heapify_up(int[] array,int size,int index){
         int i =index;
         while(i>0){
-            if(array[i]>array[(Math.ceil(i/2)-1)]){
+            if(array[i]>array[((i/2))]){
                 int temp = array[i];
-                array[i]=array[(Math.ceil(i/2)-1)];
-                array[(Math.ceil(i/2)-1)]=temp;
+                array[i]=array[(int)((i/2))];
+                array[((i/2))]=temp;
+                
             }
-            i=Math.ceil(i/2)-1;
+            else{
+                break;
+            }
+            i=(i/2);
             
         }
 
 
 
     }
-    private static void heapify_down(int[] array,int size,int index){
+     static private void heapify_down(int[] array,int size,int index){
     int i= index;
     int l;
     int r;
+
     while(2*i+2<size){
          l=2*i+1;
          r=2*i+2;
@@ -36,8 +43,13 @@ public class MaxHeap {
         if(max!=i)
          {int temp = array[max];
          array[max]=array[i];
-         array[i]=temp;}
-         i=max;
+         array[i]=temp;
+
+         i=max;}
+         else 
+         {
+          break;
+         }
 
 
 
