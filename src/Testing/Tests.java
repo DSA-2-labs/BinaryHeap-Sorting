3c44d1cdf9b1a2bd56244c4333dcd805d26b5e5e
Mahmoud Ghlab <randyorten9055@gmail.com>
2023-05-27 04:21:01 +0300

    Add tests

---
+++ src/Testing/Tests.java
@@ -0,0 +1,84 @@
+package Testing;
+
+import org.junit.Test;
+
+public class Tests {
+
+
+
+    @Test
+    public void test1()
+    {
+
+    }
+    @Test
+    public void test2()
+    {
+
+    }
+    @Test
+    public void test3()
+    {
+
+    }
+    @Test
+    public void test4()
+    {
+
+    }
+    @Test
+    public void test5()
+    {
+
+    }
+    @Test
+    public void test6()
+    {
+
+    }
+    @Test
+    public void test7()
+    {
+
+    }
+    @Test
+    public void test8()
+    {
+
+    }
+    @Test
+    public void test9()
+    {
+
+    }
+    @Test
+    public void test10()
+    {
+
+    }
+    @Test
+    public void test11()
+    {
+
+    }
+    @Test
+    public void test12()
+    {
+
+    }
+    @Test
+    public void test13()
+    {
+
+    }
+    @Test
+    public void test14()
+    {
+
+    }
+    @Test
+    public void test15()
+    {
+
+    }
+}
