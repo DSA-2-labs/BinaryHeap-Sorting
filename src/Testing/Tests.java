b13c49f931b4985645d4ad448dfdc2dc49f5f9a5
Mahmoud Ghlab <randyorten9055@gmail.com>
2023-05-27 14:37:15 +0300

    Update Tests

--- src/Testing/Tests.java
+++ src/Testing/Tests.java
@@ -1,84 +1,135 @@
 package Testing;
 
+import FileReader.Freader;
+import Sorting.Sort_Array;
 import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
 
 public class Tests {
 
+    private int[] unsorted_arr;
+    private int[] expected;
 
-
+    private Sort_Array sorting;
     @Test
-    public void test1()
+    public void test1()// 10 elements
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-10.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-10.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test2()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-20.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-20.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test3()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-30.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-30.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test4()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-50.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-50.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test5()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-100.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-100.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test6()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-200.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-200.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test7()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-300.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-300.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test8()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-500.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-500.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test9()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-1000.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-1000.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test10()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-3000.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-3000.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test11()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-5000.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-5000.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test12()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-7000.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-7000.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test13()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-10000.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-10000.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test14()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-13000.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-13000.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
     @Test
     public void test15()
     {
-
+        sorting=new Sort_Array("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/test-15000.txt");
+        unsorted_arr = sorting.getArr();
+        expected = Freader.read("/home/mahmoud/IdeaProjects/BinaryHeap-Sorting/Expected/extest-15000.txt");
+        assertArrayEquals(expected,(int[])sorting.simpleSort(true));
     }
 }
