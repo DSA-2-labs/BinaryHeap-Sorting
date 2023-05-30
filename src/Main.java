e60206b73954a53280c296e25d542b7079d465fc
Mohamed <mo.hassan2792@gmail.com>
2023-05-30 16:52:48 +0300

    finaaaaaaal

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,5 @@ public class Main {
     public static void main(String[] args) {
         cli c = new cli();
         c.i();
-//        long currentTimeMillis = System.currentTimeMillis();
-//        for(int i=0 ; i<5 ; i++) {
-//            Sort_Array x = new Sort_Array("C:\\Users\\DELL\\Desktop\\generate\\avg\\Test10.txt\\");
-//            x.heapSort(true);
-//        }
-//        long currentTimeMillis2 = System.currentTimeMillis();
-//        System.out.println("Time = "+(currentTimeMillis2-currentTimeMillis)/5.0);
     }
 }
